73df1b90b1fe0a90b65f1fcd16e6cec6d6dec49b
nyabangWech <nyabangwech@gmail.com>
2024-03-21 22:42:33 +0300

    added files

---
+++ Arrays.js
@@ -0,0 +1,22 @@
+
+let arr1=[3,34,90,12];
+console.log('last item',arr1[arr1.length-1]);
+
+let  arr2=[true,"green","where",12,156];
+console.log('last item',arr2[arr2.length-1]);
+
+ const pet=["cow","bird","snake","dog"];
+ let mypet = pet.toString();
+ console.log(mypet);
+
+ var  item =[-5 ,9 ,5,3 ,2,-3,6,8,4,1];
+ item.sort(function(a,b){return a -b});
+ console.log(item);
+
+ item.sort(function(a,b){return b -a});
+ console.log(item);
+
+ let arr=["boy","man","girl","school","girl","woman"];
+ 
+ 
+ 
